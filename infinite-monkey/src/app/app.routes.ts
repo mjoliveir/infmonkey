b47daa4415f53664786f3fcf180b3908cffbcec8
sbm-matheus <matheus.oliveira@sbmtechnology.com.br>
2026-01-08 01:22:17 -0300

    Tentando tematizar + navbar dinâmica

--- infinite-monkey/src/app/app.routes.ts
+++ infinite-monkey/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { History } from './history/history';
 import { Home } from './home/home';
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'home', component: Home },
   { path: 'history', component: History },
 ];
--- infinite-monkey/src/app/header/header.ts
+++ infinite-monkey/src/app/header/header.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Route, Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Menubar } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink, ButtonModule],
+  imports: [CommonModule, RouterLink, ButtonModule, Menubar],
   templateUrl: './header.html',
   styleUrl: './header.css',
   standalone: true
 })
-export class Header {
+export class Header implements OnInit {
+  items: MenuItem[] | undefined;
+
+  constructor(private router: Router){}
+  showMenu = false;
+
+@HostListener('window:scroll', [])
+onScroll() {
+  this.showMenu = window.scrollY > 200; // px a partir de onde aparece
+}
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Início',
+            routerLink: 'home'
+        },
+        {
+            label: 'Histório',
+            routerLink: 'history'
+        },
+        {
+            label: 'Playground',
+            routerLink: 'home'
+        }
+    ];
+}
 
 }
